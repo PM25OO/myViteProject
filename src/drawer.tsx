c532b178d0f3b4cce9c28fbfdbc9b74aa2330eba
PM25OO <3210636476@qq.com>
2025-01-01 18:35:22 +0800

    uodate

---
+++ src/drawer.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { FloatButton, Drawer } from 'antd';
+import { ShoppingCartOutlined } from '@ant-design/icons';
+import Cart from './cart.tsx';
+
+const ButtonDrawer: React.FC = () => {
+    const [open, setOpen] = useState(false);
+
+    const showDrawer = () => {
+        setOpen(true);
+    };
+
+    const onClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <>
+            <FloatButton
+                type="primary"
+                onClick={showDrawer}
+                style={{ width: '80px', height: '80px', borderRadius: '50%' }}
+                icon={<ShoppingCartOutlined style={{ textAlign:'center',fontSize: '18px' }} />}
+            >
+                Open
+            </FloatButton>
+
+            <Drawer title="Your shopping cart" onClose={onClose} open={open}>
+                <Cart />
+            </Drawer>
+        </>
+    );
+};
+
+export default ButtonDrawer;
