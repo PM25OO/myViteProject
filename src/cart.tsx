87fbfe665d71b36da05f8568058076cb3b151fbc
PM25OO <3210636476@qq.com>
2025-01-12 19:18:35 +0800

    update

--- src/cart.tsx
+++ src/cart.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Avatar, List } from 'antd';
 
 const data = [
@@ -16,20 +16,40 @@ const data = [
   },
 ];
 
-const Cart: React.FC = () => (
-  <List
-    itemLayout="horizontal"
-    dataSource={data}
-    renderItem={(item, index) => (
-      <List.Item>
-        <List.Item.Meta
-          avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
-          title={<a>{item.title}</a>}
-          description="Ant Design, a design language for background Applications, is refined by Ant UED Team"
-        />
-      </List.Item>
-    )}
-  />
-);
+const Cart: React.FC = () => {
+  type Book = { id: number, title: string, author: string, price: number };
+  const [cart, setCart] = useState<Book[]>([]);
+  useEffect(() => {
+
+    const fetchCart = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/cart');
+        const data = await response.json();
+        setCart(data || []); // 确保我们只设置 cart 数组
+      } catch (error) {
+        console.error('Error fetching cart:', error);
+        setCart([]);
+      }
+    };
+
+    fetchCart();
+  }, []);
+
+  return (
+    <List
+      itemLayout="horizontal"
+      dataSource={data}
+      renderItem={(item, index) => (
+        <List.Item>
+          <List.Item.Meta
+            avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
+            title={<a>{item.title}</a>}
+            description="Ant Design, a design language for background Applications, is refined by Ant UED Team"
+          />
+        </List.Item>
+      )}
+    />
+  );
+};
 
 export default Cart;
